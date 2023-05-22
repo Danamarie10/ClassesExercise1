0e6d0a185bbc1b201253d7a87f034b3444920ae7
Danamarie10 <dana.patrick10@outlook.com>
2023-05-22 11:53:54 -0400

    Finished exercise 1

---
+++ ClassesExercise`/ClassesExercise`/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise_
+{
+    public class Car
+   {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public void MyCar()
+        {
+            Console.WriteLine($"My car is a {Make} {Model} and the year is {Year}.");
+        }
+    }
+    
+}
---
+++ ClassesExercise`/ClassesExercise`/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Xml.Schema;
+
+namespace ClassesExercise_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+
+            myCar.Make = "Chevy";
+            myCar.Model = "Malibu";
+            myCar.Year = 2017;
+
+            myCar.MyCar();
+
+        }
+    }
+}
